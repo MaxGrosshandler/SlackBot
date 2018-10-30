94a8402cde9a8bc69063250c68cf394dd4622f36
Max Grosshandler <maxgrosshandler@gmail.com>
2018-10-30 09:48:14 -0500

    added a dice rolling command

---
+++ commands/roll.js
@@ -0,0 +1,19 @@
+const main = require("../index.js")
+var droll = require('droll');
+
+
+const slack = main.slack;
+module.exports = {
+    func: async (data, stuff) => {
+        let c = data.channel
+        var result = droll.roll(stuff[0])
+        console.log(result)
+        if (result) slack.sendMsg(c, `The result of ${stuff[0]} was: ` + result.total);
+        else {
+            slack.sendMsg(c, "That isn't a valid roll!")
+        }
+    },
+    name: "roll"
+  };
+
+
