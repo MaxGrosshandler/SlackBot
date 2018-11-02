f6c0c32728c4ea338241ba02cf49a8cfe03152fb
Max Grosshandler <maxgrosshandler@gmail.com>
2018-11-02 08:41:10 -0500

    added eval command

---
+++ commands/eval.js
@@ -0,0 +1,25 @@
+const main = require("../index.js")
+const slack = main.slack;
+module.exports = {
+  func: async (data, stuff) => {
+      let toExecute;
+      let c = data.channel
+      let code = stuff.join(" ");
+      if (code.split("\n").length === 1)
+        toExecute = eval(`async () => ${code}`);
+      else toExecute = eval(`async () => { ${code} }`);
+      toExecute.bind(this);
+      try {
+        slack.sendMsg(c, "The result was: " + await toExecute());
+      } catch (err) {
+        slack.sendMsg(c,err.stack);
+      }
+  },
+
+  options: {
+    description: "Owner-only eval",
+    fullDescription: "You don't get to use this unless you're me",
+    usage: "`sk eval`"
+  },
+  name: "eval"
+};
