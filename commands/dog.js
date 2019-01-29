fe1757eb8a46e39c6fbc90c8c6a3832a58aeba85
Max Grosshandler <maxgrosshandler@gmail.com>
2019-01-29 08:53:21 -0600

    added some comments

--- commands/dog.js
+++ commands/dog.js
@@ -1,6 +1,7 @@
 const main = require("../index.js")
 const slack = main.slack;
 // This command serves to post an image of a dog, because dogs are great
+// Currently, the image is static. I might make it dynamic in the future.
 module.exports = {
     func: async (data) => {
       slack.sendMsg(data.channel, "https://www.pexels.com/photo/adorable-animal-breed-canine-356378/");
